86cd5f64f0c89a007187a360b5421cbb2ecd48b8
Andrea <anpel1993@gmail.com>
2025-11-28 10:48:23 +0100

    fix: remove duplicate error handling in MongoDB connection

--- src/mongo-connection.ts
+++ src/mongo-connection.ts
@@ -3,13 +3,6 @@ import { MONGO_URI } from './config/env';
 
 const uri: string = MONGO_URI;
 
-
-const mongoConnection = mongoose.connect(uri, {
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Connection error to MongoDB:', err);
-});
+const mongoConnection = mongoose.connect(uri);
 
 export default mongoConnection;
-
